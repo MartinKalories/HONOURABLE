ae784819fbe6892044ce85d1f6cc02927f462061
MartinKalories <manavaadikalra@gmail.com>
2026-04-01 16:06:21 +1100

    Update optimise_model.py

--- optimise_model.py
+++ optimise_model.py
@@ -61,8 +61,8 @@ def main():
     res = gp_minimize(
         func=objective,
         dimensions=space,
-        n_calls=25,
-        n_initial_points=8,
+        n_calls=20,
+        n_initial_points=6,
         acq_func='EI',
         random_state=42
     )
