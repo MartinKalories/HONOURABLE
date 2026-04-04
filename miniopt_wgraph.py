a738e54429ae8e73a1d0f46260bb0ebbd9955a48
MartinKalories <manavaadikalra@gmail.com>
2026-04-04 17:20:45 +1100

    Update miniopt_wgraph.py

--- miniopt_wgraph.py
+++ miniopt_wgraph.py
@@ -182,8 +182,8 @@ def main():
         res = gp_minimize(
             func=objective,
             dimensions=space,
-            n_calls=30,
-            n_initial_points=10,
+            n_calls=50,
+            n_initial_points=12,
             acq_func='EI',
             random_state=42,
             callback=[VerboseCallback(n_total=15)]
