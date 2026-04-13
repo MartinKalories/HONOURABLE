db7184ec131ea2a5338ac10a25a05fb39d695aef
MartinKalories <manavaadikalra@gmail.com>
2026-04-13 13:38:13 +1000

    Update miniopt_wgraph.py

--- miniopt_wgraph.py
+++ miniopt_wgraph.py
@@ -258,7 +258,7 @@ def main():
             func=objective,
             dimensions=space,
             n_calls=40,
-            n_initial_points=12,
+            n_initial_points=8,
             acq_func='EI',
             random_state=42,
             callback=[VerboseCallback(n_total=40)]
