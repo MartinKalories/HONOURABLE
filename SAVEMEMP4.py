e521de35eceee6799e144c1211429464636e34ed
MartinKalories <manavaadikalra@gmail.com>
2026-06-25 14:38:54 +1000

    Update SAVEMEMP4.py

--- SAVEMEMP4.py
+++ SAVEMEMP4.py
@@ -20,7 +20,7 @@ from optim_space import space
 # Keep this the same to resume a crashed run.
 # Change this when starting a new experiment.
 # ------------------------------------------------------------
-RUN_ID = "trialoptspace"
+RUN_ID = "bayesopt_current_nosub_noLR_all_trials2"
 
 save_dir = datadir
 os.makedirs(save_dir, exist_ok=True)
