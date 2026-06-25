86007418ed6b6989a7986c2381b9d1209051dc8a
MartinKalories <manavaadikalra@gmail.com>
2026-06-25 12:11:08 +1000

    Update optim_space.py

--- optim_space.py
+++ optim_space.py
@@ -1,16 +1,16 @@
 from skopt.space import Real, Integer, Categorical
 
 space = [
-    Real(1e-5, 5e-3, prior="log-uniform", name="learningRate"),
+    #Real(1e-5, 5e-3, prior="log-uniform", name="learningRate"),
     Real(0.0, 0.4, name="dropout_rate"),
     Real(0.0, 0.6, name="dropout_rate_dense"),
     Real(0.0, 0.8, name="dropout_rate_psf"),
     Integer(512, 4000, name="n_units_dense"),
      #Categorical([3, 5, 7], name="ksz_enc"),
-    #Categorical([3, 5], name="ksz_psf"),
-    #Categorical([3, 5], name="ksz_wf"),
+    Categorical([3, 5], name="ksz_psf"),
+    Categorical([3, 5], name="ksz_wf"),
      #Categorical([64, 96, 128], name="nfilts_enc"),
-    #Categorical([32, 64, 96], name="nfilts_psf"),
-    #Categorical([32, 64, 96], name="nfilts_wf"),
+    Categorical([32, 64, 96], name="nfilts_psf"),
+    Categorical([32, 64, 96], name="nfilts_wf"),
      #Categorical(["relu", "elu", "gelu"], name="actFunc"),
 ]
