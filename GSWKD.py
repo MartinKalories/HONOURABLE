9a4ad7c3d7c18fa8a5f87a3e9a1d21de1346783c
MartinKalories <manavaadikalra@gmail.com>
2026-06-29 15:16:33 +1000

    Update GSWKD.py

--- GSWKD.py
+++ GSWKD.py
@@ -15,7 +15,7 @@ from optim_space import space
 # File path
 # ==================================================
 DATA_DIR = Path("/home/manav/PL-NN-testdata_forDec2025/")
-DEFAULT_CSV = "bayesopt_current_nosub_noLR_all_trials.csv"
+DEFAULT_CSV = "bayesopt_current_nosub_noLR_all_trials2_all_trials.csv"
 
 csv_path = DATA_DIR / DEFAULT_CSV
 df = pd.read_csv(csv_path)
