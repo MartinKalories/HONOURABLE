a5ec769cdac5f42f64ac45c66d78ed5c62e1a56c
MartinKalories <manavaadikalra@gmail.com>
2026-07-08 15:47:19 +1000

    Update nyquilwfmatching.py

--- nyquilwfmatching.py
+++ nyquilwfmatching.py
@@ -49,7 +49,7 @@ WAVELENGTH = 1.55
 CORE_RADIUS = 32.8 / 2
 
 # LP-mode generation and fitting.
-N_MODES = 17
+N_MODES = 20
 N_TEST = 5
 NPIX = 200          # lanternfiber returns a 2*NPIX by 2*NPIX mode image
 MAX_R = 2           # outer calculation radius in units of the core radius
