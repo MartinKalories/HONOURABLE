455e6c3c763f436713f51f591bd88cc3d3cc7554
MartinKalories <manavaadikalra@gmail.com>
2026-07-30 14:34:40 +1000

    Update GSWKD.py

--- GSWKD.py
+++ GSWKD.py
@@ -30,7 +30,7 @@ loss_col = "objective_val_loss"
 
 # Lower T makes only the best trials matter strongly.
 # Higher T gives smoother weighting across more trials.
-T = 10
+T = 1000
 
 active_dims = [dim for dim in space if dim.name is not None]
 
